e8549a974096030455ce9be2eeff212aebd9571c
ckohli-96 <ckohli@covationlabs.com>
2022-08-08 10:21:55 +0530

    Adding comments in code.

--- index.js
+++ index.js
@@ -5,30 +5,39 @@
 
 const flatten = require("./flatten");
 
-function deepSearch(keyword, artifacts, options={excluded:[]}) {
+/**
+ * It performs searching on input artifacts
+ * @param {*} keyword - search string
+ * @param {*} artifacts - nested array or object
+ * @param {*} options[optional] - excluded(array of keys that needs to be excluded while searching)
+ */
+function deepSearch(keyword, artifacts, options = { excluded: [] }) {
   let filteredKeyValue = {};
   let expandedResult = [];
+  // flatten array of object
   const flatObject = flatten(artifacts);
-console.log(flatObject)
+
+  // Extract all the keys of flatten objects
   let flatKeys = Object.keys(flatObject);
   let keys = flatKeys.map((key) => {
-	  const splitKey = key.split(".");
-	  const last = splitKey.pop();
-	  const secondLast = splitKey.pop();
-	  if(isNaN(Number(secondLast))){ 
-		  return `${secondLast}.${last}`
-	  }else{
-		  return last;
-	  }
-	});
+    const splitKey = key.split(".");
+    const last = splitKey.pop();
+    const secondLast = splitKey.pop();
+    if (isNaN(Number(secondLast))) {
+      return `${secondLast}.${last}`;
+    } else {
+      return last;
+    }
+  });
 
+  // Remove duplicate keys
   keys = [...new Set(keys)];
 
-
+  // Perform searching on nested array only on include keys.
   let filteredResults = Object.keys(flatObject).filter((k) => {
-    const shouldExclude = options.excluded.indexOf(k.split(".").pop())>-1;
-    if(shouldExclude){
-       return false;
+    const shouldExclude = options.excluded.indexOf(k.split(".").pop()) > -1;
+    if (shouldExclude) {
+      return false;
     }
     if (!flatObject[k]) {
       return false;
@@ -58,16 +67,14 @@ console.log(flatObject)
   expandedResult = [...new Set(expandedResult)];
 
   // Get key value pair
-  expandedResult = expandedResult.map(
-    (f) => {
-		if(typeof flatObject[f] !== 'undefined'){
-			filteredKeyValue[f] = flatObject[f]}
-		}
-  );
+  expandedResult = expandedResult.map((f) => {
+    if (typeof flatObject[f] !== "undefined") {
+      filteredKeyValue[f] = flatObject[f];
+    }
+  });
 
-
+  // unflatten result and return filtered data
   return flatten.unflatten(filteredKeyValue);
 }
-
 
 exports.deepSearch = deepSearch;
